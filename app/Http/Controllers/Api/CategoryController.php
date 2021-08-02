3e36fcb78eac9300b24fb3ea6185c8ccf6abd7e6
cristianvicol <cvicol2@gmail.com>
2021-08-02 15:53:59 +0300

    dev

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\m;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\m  $m
+     * @return \Illuminate\Http\Response
+     */
+    public function show(m $m)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\m  $m
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(m $m)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\m  $m
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, m $m)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\m  $m
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(m $m)
+    {
+        //
+    }
+}
