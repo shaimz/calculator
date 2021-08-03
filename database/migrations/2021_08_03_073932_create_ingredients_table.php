456d9259b27846597d7b638da044dbbf3ec15777
cristianvicol <cvicol2@gmail.com>
2021-08-03 18:30:40 +0300

    dev

---
+++ database/migrations/2021_08_03_073932_create_ingredients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredient', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('category_id');
+            $table->string('name');
+            $table->integer('price');
+            $table->string('measure');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingredients');
+    }
+}
