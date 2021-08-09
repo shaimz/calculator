13182e2804e825240bb3c3a1c4243dab8b8b7ea1
cristianvicol <cvicol2@gmail.com>
2021-08-09 18:05:25 +0300

    dev

---
+++ app/Models/Measurement.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Measurement extends Model
+{
+    public $timestamps = false;
+    protected $table = 'measurement';
+}
