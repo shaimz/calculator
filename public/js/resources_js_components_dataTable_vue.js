8d096ab09eb20e3edc3b2a6020496deff5b96ec6
CristianV350 <cristian.vicol@tarainteractive.com>
2022-12-13 11:55:18 +0200

    Gitignore edit

--- public/js/resources_js_components_dataTable_vue.js
+++
@@ -1,4 +0,0 @@
-(self.webpackChunk=self.webpackChunk||[]).push([["resources_js_components_dataTable_vue"],{"./resources/js/components/dataTable.vue":
-/*!***********************************************!*\
-  !*** ./resources/js/components/dataTable.vue ***!
-  \***********************************************/()=>{eval("\n\n//# sourceURL=webpack:///./resources/js/components/dataTable.vue?")}}]);
